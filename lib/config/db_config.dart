1fa75ada982c8a46d1690f20afa63472e618b990
NguyenHieu-class <22010103@st.phenikaa-uni.edu.vn>
2025-10-19 20:40:38 +0700

    config db

--- lib/config/db_config.dart
+++ lib/config/db_config.dart
@@ -1,9 +1,9 @@
 class DbConfig {
   /// Replace with the actual Laragon host IP address.
-  static const String host = '192.168.1.100';
+  static const String host = '127.0.0.1';
   static const int port = 3306;
-  static const String user = 'pos_user';
-  static const String password = 'TODO_CHANGE_THIS_PASSWORD';
+  static const String user = 'root';
+  static const String password = '';
   static const String database = 'pos_db';
 
   const DbConfig._();
